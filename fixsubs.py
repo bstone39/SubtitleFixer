5ec64def1320edf5d262ff05727de4ca5664d6db
Brenden <jyrcht@gmail.com>
2023-02-25 23:28:05 +0000

    Changed / added functionality as needed.
    Can now specify which line number to start on, and choose to either subtract or add a fixed number of seconds
    
    Improved the filenames for input and output
    
    Signed-off-by: Brenden <jyrcht@gmail.com>

--- fixsubs.py
+++ fixsubs.py
@@ -1,8 +1,9 @@
 import sys
 import glob
+import os
 
-def parseTime(timestamp):
-    '''Parses a subtitle file time stamp into an int'''
+def parseTime(timestamp, mode, amount):
+    '''Parses a subtitle file time stamp into an int, does logic, returns modified time'''
     hours = timestamp[:2]
     minutes = timestamp[3:5]
     seconds = timestamp[6:12]
@@ -14,7 +15,11 @@ def parseTime(timestamp):
     time += int(seconds.replace(',',''))
 
     time = int(time)
-    time -= int(sys.argv[2])
+
+    if mode == "-":
+        time -= amount
+    if mode == "+":
+        time += amount
 
     h = time // 60 // 60 // 1000 
     m = time // 60 // 1000 % 60
@@ -44,21 +49,24 @@ def parseTime(timestamp):
 
     return ts
 
-subfile = open(''.join(glob.glob("og/"+sys.argv[1])), 'r', encoding="utf-8")
-outfile = open("out/"+''.join(glob.glob("og/"+sys.argv[1]))[2:], 'w', encoding='utf-8')
+subfile = open(''.join(glob.glob(sys.argv[1])), 'r', encoding="utf-8")
+outfile = open("out/fixed_"+os.path.basename(''.join(glob.glob(sys.argv[1]))), 'w', encoding='utf-8')
+
+mode = sys.argv[3][0]
+amount = sys.argv[3][1:]
+curline = int(sys.argv[2])
 
 data = subfile.readlines()
 
 i = 1
-curline = 1
 edit_flag = False
 
 for line in data:
     if edit_flag:
         ts1, ts2 = line[:12], line[17:]
 
-        ts1 = parseTime(ts1)
-        ts2 = parseTime(ts2)
+        ts1 = parseTime(ts1, mode, int(amount))
+        ts2 = parseTime(ts2, mode, int(amount))
 
         tsstr = ts1 + " --> " + ts2 + '\n'
         outfile.write(tsstr)
